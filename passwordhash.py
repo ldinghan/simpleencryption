7153ca340234c2004339e5ef04df9751e430d872
ldinghan <86869693+ldinghan@users.noreply.github.com>
2022-03-11 11:43:16 +0800

    Update passwordhash.py

--- passwordhash.py
+++ passwordhash.py
@@ -1,5 +1,5 @@
 import random
-#pwd = input("what is your password")
+
 break1 = ["!","@","#","$","%","^","&","*"]
 break2 = ["1","2","3","4","5","6","7","8","9","0"]
 for i in range(65,91):
@@ -13,11 +13,16 @@ for i in range(97,123):
 def encrypt(s):
 	shift = random.randint(1,9)
 	output = ""
+	for i in range(10):
+		output += random.choice(break2)
+	shift2 = output[shift]
 	for i in range(len(s)):
 		temp = 0
-		temp += (len(s)-i) * shift * ord(s[i]) + shift
+		temp += (len(s)-i) * shift * ord(s[i]) + shift * ord(shift2)
 		output += str(temp) + random.choice(break1) + random.choice(break2)
-	output += str(shift) + "\n"
+	if shift < 4:
+		output += "0"
+	output += str(shift**2) + random.choice(break2) + random.choice(break2) + "\n"
 	with open("passwords.txt", "a") as file_object:
 		file_object.write(output)
 
@@ -26,7 +31,8 @@ def encrypt(s):
 
 
 def decrypt(h):
-	shift = int(h[-1])
+	shift = int(int(h[-4]+h[-3])**0.5)
+	shift2 = h[shift]
 	output = ""
 	length = 0
 	temp = ""
@@ -34,7 +40,7 @@ def decrypt(h):
 	for i in h:
 		if i in break1:
 			length += 1
-	pointer = 0
+	pointer = 10
 	while pointer < len(h):
 		if h[pointer] in break1:
 			chars.append(int(temp))
@@ -45,7 +51,7 @@ def decrypt(h):
 			pointer += 1
 	
 	for i,num in enumerate(chars):
-		char = chr(int((num-shift) / shift / (length-i))) 
+		char = chr(int((num-shift*ord(shift2)) / shift / (length-i))) 
 		output += char
 	return output
 
@@ -55,8 +61,6 @@ def reveal():
 		lines = file.readlines()
 		for line in lines:
 			print(decrypt(line.rstrip()))
-
-
 
 
 
